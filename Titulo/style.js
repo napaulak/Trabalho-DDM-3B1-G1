bea61b8a534d9f7d910d3cb81c4b241e256c5204
napaulak <anapaulaferreira1b1@gmail.com>
2024-11-07 10:20:49 -0300

    trabalhoo

---
+++ Titulo/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+ BoxTitle: {
+ alignItems: "center",
+ fontFamily: "sans",
+ fontSize: "20", 
+ fontStyle: "italic",
+ },
+ 
+ texTitle: {
+ color: "#8B0000",
+ },
+});
+export default styles
